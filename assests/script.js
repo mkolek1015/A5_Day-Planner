cbcc74970cf04d7a734bd0fcc2f7c60291919a66
mkolek1015 <mkolek7@yahoo.com>
2020-09-09 22:24:58 -0400

    final push with working time slots

--- assests/script.js
+++ assests/script.js
@@ -1,7 +1,32 @@
 
-// can't get this to work. What am I doing wrong?
-const today = moment();
-console.log(
-  "Today's date is: " + 
-  today.format('YYYY-MM-DD')
-);
+
+$(document).ready(function() {
+
+  // can't get this to work. What am I doing wrong?
+  const today = moment();
+  console.log(
+    "Today's date is: " + 
+    today.format('YYYY-MM-DD')
+  );
+
+    console.log($(".saveIcon"));
+
+    $.each( $(".saveIcon"), function(a, icon) {
+
+      $(icon).on("click", function() {
+        console.log ("we're here");
+        console.log($(this));
+
+        var id = $(this).attr("save-id");
+        console.log(id);
+        var toTarget = "#input-" + id;
+
+        var textToSave = $(toTarget).val()
+        console.log(textToSave);
+
+
+
+      })
+
+  })
+});
